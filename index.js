4a6d07fbceb69d8d00abd353c9ff4eaf1832371c
Manicharan Kollipara <manicharan@Manicharans-Laptop.local>
2024-08-05 20:04:59 +0530

    Moving UDP code to a new file and using urlparser, buffer and dgram modules

--- index.js
+++ index.js
@@ -2,6 +2,19 @@
 
 import * as fs from "fs";
 import bencode from "bencode";
+import * as dgram from "dgram";
+import Buffer from "buffer";
+import * as urlmod from "url";
 
 const torrent = bencode.decode(fs.readFileSync("puppy.torrent"));
-console.log(torrent.announce.toString("utf8"));
+const url = urlmod.parse(torrent.announce.toString("utf8"));
+console.log(url);
+const socket = dgram.createSocket("udp4");
+
+const myMsg = Buffer.Buffer.from("hello?", "utf8");
+
+socket.send(myMsg, 0, myMsg.length, 3000, "127.0.0.1", () => {});
+
+socket.on("message", (msg) => {
+  console.log("message is", msg);
+});
