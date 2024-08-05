a8da69cdd0034cac2e5ecd846311580bbc82f910
Manicharan Kollipara <manicharan@Manicharans-Laptop.local>
2024-08-05 19:36:53 +0530

    Added bencode and decoded a .torrent file

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+import * as fs from "fs";
+import bencode from "bencode";
+
+const torrent = bencode.decode(fs.readFileSync("puppy.torrent"));
+console.log(torrent.announce.toString("utf8"));
